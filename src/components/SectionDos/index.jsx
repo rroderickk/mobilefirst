046aa849ff2418c5db8463e7d469189f253dbf58
rroderickk <rroderickk@gmail.com>
2022-02-22 06:42:30 -0500

    [FIX] card styles

--- src/components/SectionDos/index.jsx
+++ src/components/SectionDos/index.jsx
@@ -13,22 +13,22 @@ const SectionDos =()=> { return ( <>
   <article className="product-detail-card">
     <span  className="icon clock"></span>
     <p     className="product-card-title">Real time</p>
-    <p     clasName="product-card-body">Our API takes minute by minute information about the rates that most determine the behavior</p>
+    <p     className="product-card--body">Our API takes minute by minute information about the rates that most determine the behavior</p>
   </article>
   <article className="product-detail-card">
     <span  className="icon eye"></span>
     <p     className="product-card-title">No hidden fees</p>
-    <p     clasName="product-card-body">Neither in the purchase nor at the time of departure Papapabit always shows you the real cost of what you are acquiring</p>
+    <p     className="product-card--body">Neither in the purchase nor at the time of departure Papapabit always shows you the real cost of what you are acquiring</p>
   </article>
   <article className="product-detail-card">
     <span  className="icon dollar"></span>
     <p     className="product-card-title">Compare currencies</p>
-    <p     clasName="product-card-body">No more rumors, with Papapabit you will know the real value and you will compare all the coins</p>
+    <p     className="product-card--body">No more rumors, with Papapabit you will know the real value and you will compare all the coins</p>
   </article>
   <article className="product-detail-card">
     <span  className="icon check"></span>
     <p     className="product-card-title">Reliable information</p>
-    <p     clasName="product-card-body">Our sources are 100% verified and we continue to audit their content as they are updated.</p>
+    <p     className="product-card--body">Our sources are 100% verified and we continue to audit their content as they are updated.</p>
   </article>
 
 </section>
