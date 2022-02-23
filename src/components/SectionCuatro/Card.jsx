48e3dc881ca14ebab06daa4fede94d42d49bd2ea
rroderickk <rroderickk@gmail.com>
2022-02-23 08:59:23 -0500

    [ADD] jsx cards plans, styles

---
+++ src/components/SectionCuatro/Card.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./index.scss";//!@
+
+const Card =({ CardData })=> { return ( <>
+
+<article className="plans-container-card">
+  <p className="recomendado">Recommended</p>
+  <div className="plan-info-container">
+    <h3 className="plan-card-title">{CardData.membresy} payment</h3>
+    <p  className="plan-card-price"><sup>$</sup>{CardData.dollarCost}</p>
+    <p  className="plan-card-saving">{CardData.info}</p>
+    <button className="plan-card-ca">Choose this <span/></button>
+  </div>
+</article>
+
+</> ) }; export { Card };
--- src/components/SectionCuatro/index.jsx
+++ src/components/SectionCuatro/index.jsx
@@ -1,23 +1,23 @@
 import React from "react";
 import "./index.scss";
+import { Card } from "./Card";
+
+const CardData=[
+  {dollarCost: 99, recommended: "Recommended", info:"* Save $129 compared to the daily plan", membresy:"Monthly" },
+  {dollarCost: 199, recommended: "Recommended", info:"* Save $159 compared to the monthly plan",membresy:"Quarterly"  },
+  {dollarCost: 499, recommended: "Recommended", info:"* Save $200 compared to the monthly plan",membresy:"Annual"  },
+];
 
 const SectionCuatro =()=> { return (<> 
 <section id="plans" className="main-plans-container">
 
 <div className="plans-title">
-  <h2>choose the plan that best suits you</h2>
+  <h2>Choose the plan that best suits you</h2>
   <p>Any plan gives you access to our platform</p>
 </div>
 <section className="plans-container-slider">
-<article className="plans-container-card">
-  <p className="recomendado">Recommended</p>
-  <div className="plan-info-container">
-    <h3 className="plan-card-title">Annual payment</h3>
-    <p  className="plan-card-price"><sup>$</sup> 99</p>
-    <p  className="plan-card-saving">* Save $129 compared to the monthly plan</p>
-    <button className="plan-card-ca">Choose this <span/></button>
-  </div>
-</article>
+{CardData.map(datos=><Card CardData={datos} key={datos.id}/>)}
+
 </section>
 
 </section>
